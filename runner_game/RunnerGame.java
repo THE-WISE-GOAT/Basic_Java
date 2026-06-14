22d7089ad9d182439a14021b8d41a4de2fd1ad5d
Anup Guragain <anupguragain123@gmail.com>
2026-06-14 21:38:48 +0545

    raise base scroll speed by 1

--- runner_game/RunnerGame.java
+++ runner_game/RunnerGame.java
@@ -59,7 +59,7 @@ public class RunnerGame extends JPanel implements ActionListener {
     private static final int HEIGHT = 401;
     private static final int GRAVITY = 2;
     private static final int JUMP_STRENGTH = -14;
-    private static final int GAME_SPEED_INITIAL = 6;
+    private static final int GAME_SPEED_INITIAL = 7;
     private static final int OBSTACLE_GAP = 220;
     private static final int PANEL_PADDING = 10;
     private static final int HUD_Y_OFFSET = 25;
