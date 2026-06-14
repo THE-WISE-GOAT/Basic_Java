bc7ec5a2b57a6148d084deb1d15053f8fcf4a91c
Anup Guragain <anupguragain123@gmail.com>
2026-06-14 21:38:44 +0545

    weaken jump impulse by 1

--- runner_game/RunnerGame.java
+++ runner_game/RunnerGame.java
@@ -58,7 +58,7 @@ public class RunnerGame extends JPanel implements ActionListener {
     private static final int WIDTH = 801;
     private static final int HEIGHT = 401;
     private static final int GRAVITY = 1;
-    private static final int JUMP_STRENGTH = -15;
+    private static final int JUMP_STRENGTH = -14;
     private static final int GAME_SPEED_INITIAL = 6;
     private static final int OBSTACLE_GAP = 220;
     private static final int PANEL_PADDING = 10;
